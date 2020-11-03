bf6839d01f10990bdc38ece85ac04b7a88e6965d
Arturo Hurtado <jart1221@gmail.com>
2020-11-02 21:55:52 -0500

    changes from stranger thigs to neon genesis evangelion, change in html and css, change names

--- precache-manifest.5a6c2d39e99f9e22b36ec3e56966c8c7.js
+++ precache-manifest.5a6c2d39e99f9e22b36ec3e56966c8c7.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "98b8fc25f6441106fe941b4fb65622ac",
-    "url": "/stranger-things-imdb/index.html"
+    "url": "/./index.html"
   },
   {
     "revision": "ee5e72ef974f89d88bcf",
-    "url": "/stranger-things-imdb/static/css/main.9739341e.chunk.css"
+    "url": "/./static/css/main.9739341e.chunk.css"
   },
   {
     "revision": "41b9863699e08563bc78",
-    "url": "/stranger-things-imdb/static/js/2.560b5b3a.chunk.js"
+    "url": "/./static/js/2.560b5b3a.chunk.js"
   },
   {
     "revision": "ee5e72ef974f89d88bcf",
-    "url": "/stranger-things-imdb/static/js/main.5b590033.chunk.js"
+    "url": "/./static/js/main.5b590033.chunk.js"
   },
   {
     "revision": "204a7e0bef095729f7cb",
-    "url": "/stranger-things-imdb/static/js/runtime~main.8234e6d8.js"
+    "url": "/./static/js/runtime~main.8234e6d8.js"
   },
   {
     "revision": "621c4151647e1ace00b0c6853a0e321b",
-    "url": "/stranger-things-imdb/static/media/imdb.621c4151.png"
+    "url": "/./static/media/imdb.621c4151.png"
   },
   {
     "revision": "e0cd63a11e7ef5c7dd6c3cdf859ebb14",
-    "url": "/stranger-things-imdb/static/media/img1.e0cd63a1.png"
+    "url": "/./static/media/img1.e0cd63a1.png"
   },
   {
     "revision": "dc10674642052e7835a81ed427fff2e5",
-    "url": "/stranger-things-imdb/static/media/img2.dc106746.jpg"
+    "url": "/./static/media/img2.dc106746.jpg"
   },
   {
     "revision": "23353fb8dc77f3aedec61f8be053f170",
-    "url": "/stranger-things-imdb/static/media/img4.23353fb8.jpg"
+    "url": "/./static/media/img4.23353fb8.jpg"
   },
   {
     "revision": "bb5430f4f47c9a13043ef4f9d059d973",
-    "url": "/stranger-things-imdb/static/media/img5.bb5430f4.jpg"
+    "url": "/./static/media/img5.bb5430f4.jpg"
   },
   {
     "revision": "9db53c5f94b044a7eeceebed4c54ae42",
-    "url": "/stranger-things-imdb/static/media/img6.9db53c5f.jpg"
+    "url": "/./static/media/img6.9db53c5f.jpg"
   },
   {
     "revision": "a9743edd7a21123c890a485a89c07b80",
-    "url": "/stranger-things-imdb/static/media/strangerthings-banner.a9743edd.png"
+    "url": "/./static/media/NeonGenesisEvangelionBaner.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stranger-things-imdb/precache-manifest.5a6c2d39e99f9e22b36ec3e56966c8c7.js"
+  "/./precache-manifest.5a6c2d39e99f9e22b36ec3e56966c8c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stranger-things-imdb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
